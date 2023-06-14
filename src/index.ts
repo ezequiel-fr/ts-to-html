26a45716ef0c29393410dc9934c88fe40a4fd8a6
TheRedMine TheredMaths <theredminedu51@gmail.com>
2023-06-14 18:14:23 +0200

    Replace server from index to server

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,1 @@
-#!/usr/bin/env node
-
-import { spawn } from 'child_process';
-
-function startServer() {
-    const serverProcess = spawn('node', ["server.js"], {
-        stdio: 'pipe',
-        shell: true,
-    });
-
-    serverProcess.stderr.pipe(process.stderr);
-    serverProcess.stdin.pipe(process.stdin);
-    serverProcess.stdout.pipe(process.stdout);
-
-    serverProcess.on('close', code => code !== null && process.exit(code));
-}
-
-startServer();
+require('./server');
